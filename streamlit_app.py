6d82f8b89c0930968c1c19e9cf1dc4039e8c7b89
Daniel Weigel <daniel.wgel@gmail.com>
2024-07-08 11:45:18 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import numpy as np
+import altair as alt
+import pandas as pd
+
+st.header('st.write')
+
+
+
+st.write(1234)
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+st.write('Below is a Dataframe:', df, 'Above is a dataframe.')
+
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
+
+
